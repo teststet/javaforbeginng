20e0c637576d91abd1463874bf40931bea1137ce
testest <pavlikthebestonmars@gmail.com>
2019-02-17 22:42:12 +0300

    Добавлено решение 9 задачи

---
+++ src/Task9.java
@@ -0,0 +1,24 @@
+/*Задание №9: Ввод/вывод данных для массива
+Произведите ввод данных с клавиатуры в массив,
+а после этого произведите вывод массива на экран,
+где каждый элемент массива умножается на 2.
+ */
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String args[]) {
+        int mas[] = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Введите число для массива: ");
+            Scanner in = new Scanner(System.in);
+            mas[i] = in.nextInt();
+        }
+
+        System.out.println("Вывод элементов массива умноженых на 2");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(mas[i] * 2);
+        }
+    }
+}
