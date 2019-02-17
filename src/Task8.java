1b29eae5cc49b14f9c0861cf6cfcfeaae0abb846
testest <pavlikthebestonmars@gmail.com>
2019-02-17 12:43:46 +0300

    Добавлено решение 8 задачи

---
+++ src/Task8.java
@@ -0,0 +1,15 @@
+/*
+Задача №8: Цикличный вывод данных на экран Задание
+Необходимо с помощью цикла вывести на экран числа от 1 до 50.
+ */
+public class Task8 {
+    public static void main(String args[]) {
+        int i = 1;
+        while (i < 51) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+
+}
