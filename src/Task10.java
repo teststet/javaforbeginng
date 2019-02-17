c251bbebd27b5a738181fcfd1659f0b752f2e3d8
testest <pavlikthebestonmars@gmail.com>
2019-02-17 23:11:12 +0300

    Добавлено решение 10 задачи

---
+++ src/Task10.java
@@ -0,0 +1,27 @@
+/*Задание №10: Ввод/вывод данных для матрицы
+Произведите ввод данных с клавиатуры в матрицу,
+а после этого произведите вывод первой строки матрицы на экран,
+где каждый элемент умножается на 3.
+ */
+import java.util.Scanner;
+
+public class Task10 {
+    public static void main(String args[]) {
+        int mas[][] = new int[3][3];
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                System.out.print("Введите число для элемента матрицы[" + (i+1) + "]" + "[" + (j+1) + "]: ");
+                Scanner in = new Scanner(System.in);
+                mas[i][j] = in.nextInt();
+            }
+        }
+
+        System.out.println("Вывод первой строки матрицы, где каждый элемент умножается на 3");
+
+        for(int j = 0; j < 3; j++) {
+            System.out.println("Элемент [1][" + (j+1) + "]: " + mas[0][j]*3);
+            //System.out.print(mas[0][j]*3 + " ");
+        }
+    }
+}
